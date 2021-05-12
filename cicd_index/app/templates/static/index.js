1b262218c62f1ad63c21e52a1f5a7998037534d3
Your Name <you@example.com>
2021-05-12 12:50:56 +0000

    fixed live updates

--- cicd_index/app/templates/static/index.js
+++ cicd_index/app/templates/static/index.js
@@ -2,8 +2,6 @@
 
 // start live valuen
 var current_details = null;
-setTimeout(update_live_values, 0);
-setTimeout(update_resources, 0);
 
 
 {% include "static/index_functions.js" %}
@@ -14,3 +12,6 @@ setTimeout(update_resources, 0);
 {% include "static/index_reset_form.js" %}
 {% include "static/index_settings_form.js" %}
 {% include "static/index_ui.js" %}
+
+setTimeout(update_live_values, 0);
+setTimeout(update_resources, 0);
