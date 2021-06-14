04c033d0c999c6b76a01a326d14b7b3dcd8c62c9
marc <marc@itewimmer.de>
2021-06-14 22:24:51 +0200

    requirements.txt

--- cicd_index/app/app_utils/tools.py
+++ cicd_index/app/app_utils/tools.py
@@ -409,7 +409,8 @@ def update_instance_folder(branch, rolling_file, instance_folder=None):
                     raise Exception(msg)
                 commit = repo.refs[branch].commit
                 write_rolling_log(rolling_file, f"Setting public access rights in {instance_folder}, for example requirements.txt is updated")
-                subprocess.check_call(["/usr/bin/chmod", "a+w", "-R", instance_folder / 'requirements.txt'])
+                if (instance_folder / 'requirements.txt').exists():
+                    subprocess.check_call(["/usr/bin/chmod", "a+w", "-R", instance_folder / 'requirements.txt'])
                 return str(commit)
 
             except Exception as ex:
