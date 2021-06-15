17cd6858f921b422b75778c58d7ac5274a5011e0
marc <marc@itewimmer.de>
2021-06-15 23:23:14 +0200

    remove settings

--- cicd_index/app/app_utils/cronjob_builder.py
+++ cicd_index/app/app_utils/cronjob_builder.py
@@ -175,6 +175,7 @@ def make_instance(site, use_dump):
     _odoo_framework(site, ["turn-into-dev", "turn-into-dev"])
 
     _odoo_framework(site, ["set-ribbon", site['name']])
+    _odoo_framework(site, ["remove-settings", '--settings', 'web.base.url.freeze,web.base.url'])
     _odoo_framework(site, ["prolong"])
 
 
