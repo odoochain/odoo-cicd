a8b694b35d1524e82f6d3f0575f00b16b52ad8a6
marc <marc@itewimmer.de>
2021-11-08 07:19:11 +0100

    [master]

--- odoo_admin/addons/cicd/models/commit.py
+++ odoo_admin/addons/cicd/models/commit.py
@@ -5,8 +5,9 @@ class GitCommit(models.Model):
     _name = 'cicd.git.commit'
 
     name = fields.Char("SHA", required=True)
-    branch_ids = fields.Many2many('cicd.git.branch', "Repo", required=True)
+    branch_ids = fields.Many2many('cicd.git.branch', string="Repo", required=True)
     date_registered = fields.Datetime("Date registered")
+    date = fields.Datetime("Date")
 
     _sql_constraints = [
         ('name', "unique(name)", _("Only one unique entry allowed.")),
--- odoo_admin/odoo.sh
+++ odoo_admin/odoo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export $( cat "$SCRIPT_DIR/../.env" | xargs)
 
-RESTRICT="--restrict-docker-compose ""${SCRIPT_DIR}/docker-compose.yml"" --restrict-setting ""${SCRIPT_DIR}/../.env"" --restrict-setting ""${SCRIPT_DIR}/settings"" "
+RESTRICT="--restrict-docker-compose ""${SCRIPT_DIR}/docker-compose.yml"" --restrict-setting ""${SCRIPT_DIR}/../.env"" --restrict-setting ""${SCRIPT_DIR}/settings"" --project-name ""${CICD_NETWORK_NAME}adminodoo"" "
 
 odoo $RESTRICT "$@"
 
