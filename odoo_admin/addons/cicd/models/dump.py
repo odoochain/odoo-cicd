3042f339a46438953128e5c6060dcac32fe15858
marc <marc@itewimmer.de>
2021-11-09 08:43:20 +0100

    [master]

--- odoo_admin/addons/cicd/models/dump.py
+++ odoo_admin/addons/cicd/models/dump.py
@@ -24,9 +24,23 @@ class Dump(models.Model):
             for volume in machine.volume_ids.filtered(lambda x: x.contains_dumps):
                 files = machine._execute_shell([
                     "ls", volume.name + "/"
-                ]).split("\n")
+                ]).strip().split("\n")
 
-                import pudb;pudb.set_trace()
                 for file in files:
+                    if not file:
+                        continue
                     path = volume.name + "/" + file
-                    fileobj = shell.open(path)
+                    dumps = self.sudo().with_context(active_test=False).search([('name', '=', path), ('machine_id', '=', machine.id)])
+                    if not dumps:
+                        dumps = dumps.sudo().create({
+                            'name': path,
+                            'machine_id': machine.id,
+                        })
+                    try:
+                        machine._execute_shell(['/usr/bin/test', '-f', path])
+                    except Exception:
+                        import pudb;pudb.set_trace()
+                    else:
+                        dumps.size = int(machine._execute_shell([
+                            'stat', '-c', '%s', path
+                        ]).strip())
