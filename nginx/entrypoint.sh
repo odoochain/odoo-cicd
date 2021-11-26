13b6aa539a4aa7e4d293b41faa43998b8bef94ed
cicd <cicd>
2021-11-26 16:56:53 +0100

    loose-upstreams

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${PROXY_PASS_INDEX}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
