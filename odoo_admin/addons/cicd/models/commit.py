2a43ee49da9bf8d42c98979fbcc3acd2efa9e267
marc <marc@itewimmer.de>
2021-12-03 15:14:56 +0100

    [odooadmin]

--- odoo_admin/addons/cicd/models/commit.py
+++ odoo_admin/addons/cicd/models/commit.py
@@ -11,11 +11,28 @@ class GitCommit(models.Model):
     date = fields.Datetime("Date")
     author = fields.Char("Author")
     text = fields.Text("Text")
-    test_run_ids = fields.Many2many('cicd.test.run', string="Test Runs")
+    test_run_ids = fields.Many2many('cicd.test.run', string="Test Runs", store=True)
+    test_state = fields.Selection([
+        ('success', 'Success'),
+        ('failed', 'Failed'),
+    ], compute="_compute_test_state")
+    approval_state = fields.Selection([
+        ('approved', 'Approved'),
+        ('declined', 'Declined'),
+    ])
 
     _sql_constraints = [
         ('name', "unique(name)", _("Only one unique entry allowed.")),
     ]
+
+    @api.depends('test_run_ids', 'test_run_ids.state')
+    def _compute_test_state(self):
+        for rec in self:
+            testruns = rec.test_run_ids.sorted(lambda x: x.id)
+            if not testruns or testruns[0].state == 'open':
+                rec.test_state = False
+                continue
+            rec.test_state = testruns[0].state
 
     def run_tests(self, filtered=None):
         for ttype in self.env['cicd.test.run']._get_types(filtered):
