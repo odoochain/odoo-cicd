99b1427d6286ffbea9402713c44b19b77eb4bcc3
marc <marc@itewimmer.de>
2021-12-13 18:46:05 +0100

    [master]

--- odoo_admin/addons/cicd/models/compressor.py
+++ odoo_admin/addons/cicd/models/compressor.py
@@ -18,9 +18,9 @@ class Compressor(models.Model):
     branch_id = fields.Many2one('cicd.git.branch', string="Use Branch for compression", required=True)
     date_last_success = fields.Datetime("Date Last Success")
     last_input_size = fields.Integer("Last Input Size")
-    last_input_size_human = fields.Integer("Last Input Size")
+    last_input_size_human = fields.Char("Last Input Size")
     last_output_size = fields.Integer("Last Input Size")
-    last_output_size_human = fields.Integer("Last Input Size")
+    last_output_size_human = fields.Char("Last Input Size")
     performance = fields.Integer("Performance", compute="_compute_numbers")
 
     def _ensure_cronjob(self):
@@ -58,5 +58,5 @@ class Compressor(models.Model):
                 rec.performance = 100.0 - (float(rec.last_output_size) / float(rec.last_input_size) * 100)
             else:
                 rec.performance = 0
-            rec.last_input_size_human = humanize.naturalsize(rec.last_input_size)
-            rec.last_output_size_human = humanize.naturalsize(rec.last_output_size)
+            rec.last_input_size_human = humanize.naturalsize(rec.last_input_size or 0)
+            rec.last_output_size_human = humanize.naturalsize(rec.last_output_size or 0)
