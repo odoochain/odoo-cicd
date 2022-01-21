76c60cae40971a7ed489411fe933dbd267e0d70f
cicd <cicd>
2022-01-21 10:18:54 +0000

    tests

--- odoo_admin/addons/cicd/tests/basic.py
+++ odoo_admin/addons/cicd/tests/basic.py
@@ -13,12 +13,18 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 from odoo.exceptions import UserError, RedirectWarning, ValidationError, AccessError
 
+GIT_USER = 'odoofun'
+GIT_PASSWORD = 'funtastic'
 
-class BasicRepo(common.TransactionCase):
+class TestBasicRepo(common.TransactionCase):
 
     def setUp(self):
         super().setUp()
 
     def test_setuprepo(self):
         repo = self.env['cicd.git.repo'].create({
+            'name': 'odoofun-new'
+            'url': 'https://git.itewimmer.de/odoo/customs/odoofun',
+            'username': GIT_USER,
+            'password': GIT_PASSWORD
         })
