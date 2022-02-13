fc68b95729bd3f70bf36726ec36dddbcab2f2f28
cicd2 <cicd2>
2022-02-13 15:47:03 +0000

    hotfix

--- odoo_admin/addons/cicd/models/ticketsystem.py
+++ odoo_admin/addons/cicd/models/ticketsystem.py
@@ -1,6 +1,8 @@
 import re
 from odoo import _, api, fields, models, SUPERUSER_ID
 from odoo.exceptions import UserError, RedirectWarning, ValidationError
+
+
 class TicketSystem(models.Model):
     _name = 'cicd.ticketsystem'
 
