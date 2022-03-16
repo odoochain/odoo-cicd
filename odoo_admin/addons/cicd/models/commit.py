fa5a11ff840cd1b28c81722679e769ad0386feb7
Your Name <you@example.com>
2022-03-16 21:10:59 +0000

    hotfix: fix

--- odoo_admin/addons/cicd/models/commit.py
+++ odoo_admin/addons/cicd/models/commit.py
@@ -60,7 +60,7 @@ class GitCommit(models.Model):
     def _compute_test_state(self):
         for rec in self:
             testruns = rec.test_run_ids.sorted(lambda x: x.id, reverse=True)
-            if not testruns or testruns[0].state in ('open', 'running'):
+            if not testruns or testruns[0].state in ('open', 'running', 'omitted'):
                 rec.test_state = False
                 continue
             new_state = testruns[0].state
