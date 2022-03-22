fd1ba1f67e6e27e835cb0d243bf9edb9051fc591
cicd2 <cicd2>
2022-03-22 08:30:40 +0000

    hotfix
    wiz new branch

--- odoo_admin/addons/cicd/models/wiz_new_branch.py
+++ odoo_admin/addons/cicd/models/wiz_new_branch.py
@@ -13,14 +13,33 @@ class NewBranch(models.TransientModel):
     @api.model
     def default_get(self, fields):
         res = super().default_get(fields)
-        if res.get('repo_id'):
-            repo = self.env['cicd.git.repo'].sudo().browse(res['repo_id'])
-            res['dump_id'] = repo.default_simulate_install_id_dump_id.id
-        return res
+        dump = False
 
-    @api.onchange('repo_id')
-    def _onchange_repo(self):
-        self.dump_id = self.repo_id.sudo().default_simulate_install_id_dump_id
+        if res.get('source_branch_id'):
+            source_branch = self.env['cicd.git.branch'].browse(
+                res['source_branch_id']
+            )
+            if source_branch.dump_id:
+                dump = source_branch.dump_id
+
+            elif source_branch.backup_filename:
+                for volume in source_branch.backup_machine_id.sudo().volume_ids.filtered(
+                        lambda x: x.ttype == 'dumps'):
+                    dump_name = volume.name + "/" + source_branch.backup_filename
+                    dump = source_branch.backup_machine_id.sudo()\
+                        .dump_ids.filtered(
+                            lambda x: x.name == dump_name)
+                    if dump:
+                        dump = dump[0]
+                        break
+
+        if not dump and res.get('repo_id'):
+            repo = self.env['cicd.git.repo'].sudo().browse(res['repo_id'])
+            dump = repo.default_simulate_install_id_dump_id.id
+
+        if dump:
+            res['dump_id'] = dump
+        return res
 
     @api.constrains("new_name")
     def _check_name(self):
@@ -34,11 +53,18 @@ class NewBranch(models.TransientModel):
         branch = self.source_branch_id.create({
             'name': self.new_name,
             'dump_id': self.dump_id.id,
-            'backup_machine_id': self.repo_id.sudo().machine.id,
+            'backup_machine_id': self.repo_id.sudo().machine_id.id,
             'force_prepare_dump': True,
         })
         self.with_delay()._make_branch(branch.sudo())
-        return {'type': 'ir.actions.act_window_close'}
+        return {
+            'view_type': 'form',
+            'res_model': branch._name,
+            'res_id': branch.id,
+            'views': [(False, 'form')],
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+        }
 
     def _make_branch(self, branch):
         machine = branch.repo_id.machine_id
@@ -61,12 +87,3 @@ class NewBranch(models.TransientModel):
                         "git", "push", "--set-upstream",
                         "-f", 'origin', self.new_name])
                     branch.fetch()
-
-                return {
-                    'view_type': 'form',
-                    'res_model': branch._name,
-                    'res_id': branch.id,
-                    'views': [(False, 'form')],
-                    'type': 'ir.actions.act_window',
-                    'target': 'current',
-                }
