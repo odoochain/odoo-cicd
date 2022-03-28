0d9533158a89e71d536b733d3043410ac5d19939
Your Name <you@example.com>
2022-03-28 07:53:06 +0000

    hotfix: fix projectname context

--- odoo_admin/addons/cicd/models/mixin_extra_env.py
+++ odoo_admin/addons/cicd/models/mixin_extra_env.py
@@ -15,7 +15,7 @@ class MixinExtraEnv(models.AbstractModel):
 
             # avoid long locking
             with closing(self.env.registry.cursor()) as cr:
-                env = api.Environment(cr, SUPERUSER_ID, {})
+                env = api.Environment(cr, SUPERUSER_ID, self._context)
                 env.reset()
                 obj = obj.with_env(env)
 
