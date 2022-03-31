3c46a7ab3fb34c0a6b00d34bbdfdc9361434844d
Michael Tietz <mtietz@mt-software.de>
2022-03-31 15:03:51 +0200

    added schedule views

--- odoo_admin/addons/cicd/models/mixin_schedule.py
+++ odoo_admin/addons/cicd/models/mixin_schedule.py
@@ -1,36 +1,4 @@
-from odoo import _, api, fields, models, SUPERUSER_ID
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
-from odoo.exceptions import UserError, RedirectWarning, ValidationError
-from croniter import croniter
-
-
-class CicdSchedule(models.Model):
-    _name = 'cicd.schedule'
-
-    name = fields.Char('Name', required=True)
-    schedule = fields.Char('Schedule', required=True, help='Crontab based schedule:\n*/5 * * * * => every 5 minutes\n* * * * * */10 => every 10 seconds')
-    
-    @api.model
-    def _get_next(self, schedule, start_from=None):
-        start_from = start_from or fields.Datetime.now()
-        return croniter(schedule, start_from).get_next(type(start_from))
-
-    def _compute_next(self, start_from=None):
-        self.ensure_one()
-        return self._get_next(self.schedule, start_from)
-
-    def compute_next(self, start_from=None):
-        start_from = start_from or fields.Datetime.now()
-        dates = [rec._compute_next(start_from) for rec in self]
-        return dates
-
-    def compute_next_min(self, start_from=None):
-        dates = self.compute_next(start_from)
-        return dates and min(dates)
-
-    def compute_next_max(self, start_from=None):
-        dates = self.compute_next(start_from)
-        return dates and max(dates)
+from odoo import fields, models
 
 
 class Schedule(models.AbstractModel):
---
+++ odoo_admin/addons/cicd/models/schedule.py
@@ -0,0 +1,31 @@
+from odoo import fields, models
+from croniter import croniter
+
+
+class CicdSchedule(models.Model):
+    _name = 'cicd.schedule'
+
+    name = fields.Char('Name', required=True)
+    schedule = fields.Char('Schedule', required=True, help='Crontab based schedule:\n*/5 * * * * => every 5 minutes\n* * * * * */10 => every 10 seconds')
+    
+    @api.model
+    def _get_next(self, schedule, start_from=None):
+        start_from = start_from or fields.Datetime.now()
+        return croniter(schedule, start_from).get_next(type(start_from))
+
+    def _compute_next(self, start_from=None):
+        self.ensure_one()
+        return self._get_next(self.schedule, start_from)
+
+    def compute_next(self, start_from=None):
+        start_from = start_from or fields.Datetime.now()
+        dates = [rec._compute_next(start_from) for rec in self]
+        return dates
+
+    def compute_next_min(self, start_from=None):
+        dates = self.compute_next(start_from)
+        return dates and min(dates)
+
+    def compute_next_max(self, start_from=None):
+        dates = self.compute_next(start_from)
+        return dates and max(dates)
