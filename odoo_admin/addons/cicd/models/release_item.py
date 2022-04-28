e8cc855c88766bc4acc06d8d69c4aa0df0c1f390
Marc Wimmer <marc@itewimmer.de>
2022-04-28 19:51:43 +0200

    [release_settings]

--- odoo_admin/addons/cicd/models/release_item.py
+++ odoo_admin/addons/cicd/models/release_item.py
@@ -400,7 +400,8 @@ class ReleaseItem(models.Model):
                     self.state = 'ready'
 
         elif self.state == 'ready':
-            if now > self.planned_date:
+            if (self.planned_date and now > self.planned_date) or \
+                    self.release_type == 'hotfix':
                 if self.release_id.auto_release:
                     self._do_release()
 
