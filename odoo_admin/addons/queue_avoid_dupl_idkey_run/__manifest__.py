952dba8ad8ad8512b64cc29b92f9c42cd005c872
Marc Wimmer <marc@itewimmer.de>
2022-05-04 21:10:37 +0200

    [master]

--- odoo_admin/addons/queue_avoid_dupl_idkey_run/__manifest__.py
+++ odoo_admin/addons/queue_avoid_dupl_idkey_run/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'queue_avoid_dupl_idkey_run',
     'version': '15.0.1.0',
     'author': 'Marc Wimmer (marc@itewimmer.de)',
-    'depends': ['base'],
+    'depends': ['queue_job'],
     'data': [],
     "external_dependencies": {"python": [], "bin": []},
     #"uninstall_hook": "uninstall_hook",
