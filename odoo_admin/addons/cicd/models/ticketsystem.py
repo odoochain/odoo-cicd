bc84e6a3eeb30c402d2c89210222514dccc89623
Marc Wimmer <marc@itewimmer.de>
2022-05-25 07:13:21 +0200

    [master]

--- odoo_admin/addons/cicd/models/ticketsystem.py
+++ odoo_admin/addons/cicd/models/ticketsystem.py
@@ -13,6 +13,9 @@ class TicketSystem(models.Model):
         "Regex", default=".*", required=True,
         help="Parsing branch to match ticket in ticketsystem")
 
+    test_branch_id = fields.Many2one('cicd.git.branch', string="Test Branch")
+    link_to_test_branch = fields.Char("Link to branch", compute="_compute_link")
+
     def _extract_ts_part(self, branch):
         name_orig = branch.ticket_system_ref or branch.name or ''
         if self.regex and name_orig:
@@ -27,3 +30,14 @@ class TicketSystem(models.Model):
         name = self._extract_ts_part(branch)
         url = (self.url or '') + 'browse/' + (name or "???????")
         return url
+
+    def test_ticketsystem(self):
+        self.ensure_one()
+        if not self.test_branch_id:
+            raise ValidationError("Please choose a branch")
+        self.test_branch_id._report_comment_to_ticketsystem("test")
+
+    @api.depends("test_branch_id")
+    def _compute_link(self):
+        for rec in self:
+            rec.link_to_test_branch = rec._compute_url(rec.test_branch_id)
