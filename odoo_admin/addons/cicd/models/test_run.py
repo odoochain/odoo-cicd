8646cc8241a7957bcb452a8dee56231a6c3a71bc
Marc Wimmer <marc@itewimmer.de>
2022-07-07 06:13:46 +0000

    hotfix: fix typo test run migration

--- odoo_admin/addons/cicd/models/test_run.py
+++ odoo_admin/addons/cicd/models/test_run.py
@@ -259,7 +259,7 @@ class CicdTestRun(models.Model):
             self.duration = (
                 arrow.utcnow() - arrow.get(self.date_started)
             ).total_seconds()
-            self.as_job("cleanup", True)._cleanup_testruns()
+            self.as_job("cleanup", True, eta=300)._cleanup_testruns()
 
             self.as_job("compute_success_state", True)._compute_success_state()
             self.as_job("inform_developer", True)._inform_developer()
