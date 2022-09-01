59c42e74a59905d7ac1d8ae175e353f224098e10
Marc Wimmer <marc@itewimmer.de>
2022-09-01 22:59:27 +0200

    fix migration tablename

--- odoo_admin/addons/cicd/migrations/15.0.1.5/post-migrate.py
+++ odoo_admin/addons/cicd/migrations/15.0.1.5/post-migrate.py
@@ -4,7 +4,7 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, version):
     for table in [
         "cicd_test_run_line_unittest",
-        "cicd_test_run_line_robotest",
+        "cicd_test_run_line_robottest",
         "cicd_test_run_line_migration",
     ]:
         cr.execute(f"update {table} set date_finished='1980-04-04 00:00:00'")
