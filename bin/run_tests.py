e3a522270d8d1b69c8a30cd6625ce31c40f56e69
Marc Wimmer <marc@itewimmer.de>
2022-09-19 11:07:10 +0200

    [master]

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+import sys
 import time
 import os
 import subprocess
 from pathlib import Path
 import click
 
-CICD_USER = subprocess.check_output(["whoami"], shell=True, encoding="utf8")
-CICD_HOME = os.getcwd()
+CICD_USER = subprocess.check_output(["whoami"], shell=True, encoding="utf8").strip()
+CICD_HOME = os.getcwd().strip()
 SEP = 80 * "!"
+subprocess.check_call("./cicd reload", shell=True)
+config = subprocess.check_output("./cicd config --full", encoding="utf8", shell=True)
 
-click.secho(
-    f"{SEP}\n" f"{SEP}\n" "DO NOT RUN ON PRODUCTION SYSTEM\n" f"{SEP}\n" f"{SEP}\n"
-)
-input("Press ctrl+c now to abort.")
+if not "DEVMODE: '1'" in config:
+	click.secho(
+		f"{SEP}\n" f"{SEP}\n" "DO NOT RUN ON PRODUCTION SYSTEM\n" f"{SEP}\n" f"{SEP}\n",
+		fg='red',
+	)
+	sys.exit(1)
 
 click.secho("Making sure that ssh access is possible", fg="yellow")
 id_rsa = Path("odoo_admin/tests/res/id_rsa.pub").read_text().strip()
@@ -29,6 +34,7 @@ subprocess.check_call("./cicd update", shell=True)
 subprocess.check_call("./cicd dev-env set-password-all-users 1", shell=True)
 subprocess.check_call("./cicd up -d", shell=True)
 time.sleep(10)
+import pudb;pudb.set_trace()
 subprocess.check_call(
     f"./cicd robot tests_all "
 	f"--param ROBOTTEST_SSH_USER={CICD_USER} "
