4b9134b0b11b2508c609d21062ccc530fed1e6e2
Marc Wimmer <marc@itewimmer.de>
2022-10-15 22:43:38 +0200

    Merged: https://github.com/odoo/odoo

--- odoo_admin/odoo/odoo/sql_db.py
+++ odoo_admin/odoo/odoo/sql_db.py
@@ -20,7 +20,7 @@ from decorator import decorator
 import psycopg2
 import psycopg2.extras
 import psycopg2.extensions
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_UNCOMMITTED, ISOLATION_LEVEL_READ_COMMITTED, ISOLATION_LEVEL_REPEATABLE_READ
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED, ISOLATION_LEVEL_REPEATABLE_READ
 from psycopg2.pool import PoolError
 from werkzeug import urls
 
@@ -231,7 +231,7 @@ class Cursor(BaseCursor):
     """
     IN_MAX = 1000   # decent limit on size of IN queries - guideline = Oracle limit
 
-    def __init__(self, pool, dbname, dsn, serialized=False):
+    def __init__(self, pool, dbname, dsn, serialized=True):
         super().__init__()
 
         self.sql_from_log = {}
@@ -434,7 +434,7 @@ class Cursor(BaseCursor):
             isolation_level = \
                 ISOLATION_LEVEL_REPEATABLE_READ \
                 if self._serialized \
-                else ISOLATION_LEVEL_READ_UNCOMMITTED
+                else ISOLATION_LEVEL_READ_COMMITTED
         self._cnx.set_isolation_level(isolation_level)
 
     @check
@@ -700,7 +700,7 @@ class Connection(object):
         self.dsn = dsn
         self.__pool = pool
 
-    def cursor(self, serialized=False):
+    def cursor(self, serialized=True):
         cursor_type = serialized and 'serialized ' or ''
         _logger.debug('create %scursor to %r', cursor_type, self.dsn)
         return Cursor(self.__pool, self.dbname, self.dsn, serialized=serialized)
